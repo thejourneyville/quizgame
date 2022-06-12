4d17d69a87626982392ee81ac700bf14b2e20ab8
thejourneyville <bmalberg@protonmail.com>
2022-06-12 16:46:19 -0700

    Updates

---
+++ precache-manifest.4ca8f3481852ed02dedd77d738b53f9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95ac695eee76b4dfd8f5",
+    "url": "/quizgame/static/css/main.0943d8a4.chunk.css"
+  },
+  {
+    "revision": "95ac695eee76b4dfd8f5",
+    "url": "/quizgame/static/js/main.95ac695e.chunk.js"
+  },
+  {
+    "revision": "cc2aa90f3e4474044059",
+    "url": "/quizgame/static/js/1.cc2aa90f.chunk.js"
+  },
+  {
+    "revision": "22625ca8d5852d4b35ed",
+    "url": "/quizgame/static/js/runtime~main.22625ca8.js"
+  },
+  {
+    "revision": "05250c5b15a5790e857b99a92bbf2a41",
+    "url": "/quizgame/index.html"
+  }
+];
